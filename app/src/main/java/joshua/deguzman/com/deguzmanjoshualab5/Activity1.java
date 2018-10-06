4d056966e1caf60241de552e453acaf58c380500
User-Pc <joshuamartin.deguzman.iics@ust.edu.ph>
2018-10-06 19:50:28 +0800

    Added Logcat

--- app/src/main/java/joshua/deguzman/com/deguzmanjoshualab5/Activity1.java
+++ app/src/main/java/joshua/deguzman/com/deguzmanjoshualab5/Activity1.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -28,7 +29,7 @@ public class Activity1 extends AppCompatActivity {
         map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent map = null, chooser = null;
+                Intent map, chooser;
                 map = new Intent(Intent.ACTION_VIEW);
                 map.setData(Uri.parse("geo:14.602843, 120.970385"));
                 chooser = Intent.createChooser(map, "Choose Map Application");
@@ -36,4 +37,41 @@ public class Activity1 extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("LAB5", "onStart() have executed");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("LAB5", "onResume() have executed");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("LAB5", "onPause() have executed");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("LAB5", "onStop() have executed");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("LAB5", "onDestroy() has executed");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("LAB5", "onRestart() has executed");
+    }
+
 }
